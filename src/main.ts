21329aaf8ba2698c639a1574e95fa0a9dbe431ff
jacob zlogar <jacob@vaultinnovation.com>
2023-01-12 12:24:12 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:3001"
+      ]
+    }
+  });
   await app.listen(8080);
 }
 bootstrap();
